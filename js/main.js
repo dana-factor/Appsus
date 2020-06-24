2d9d12a5339f16bf81cbb2c8724708b60df255bb
dana-factor <factor.dana@gmail.com>
2020-06-24 17:15:45 +0300

    starting

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import { myRouter } from "./routes.js";
+
+new Vue({
+  el: "#app",
+  router: myRouter,
+  template: `
+        <main>
+            <main-header></main-header>
+            <router-view />
+            <main-footer></main-footer>
+        </main>
+    `,
+});
---
+++ js/routes.js
@@ -0,0 +1,46 @@
+import homePage from './pages/home-page.cmp.js';
+import aboutUs from './pages/about-us.cmp.js';
+import bookApp from './apps/books/pages/book-app.cmp.js';
+import bookDetails from './apps/books/pages/book-details.cmp.js';
+import emailApp from './apps/email/pages/email-app.cmp.js';
+import emailDetails from './apps/email/pages/email-details.cmp.js';
+import noteApp from './apps/notes/pages/note-app.cmp.js';
+import noteDetails from './apps/notes/pages/note-details.cmp.js';
+
+
+const myRoutes = [
+    {
+        path: '/',
+        component: homePage
+    },
+    {
+        path: '/about',
+        component: aboutUs
+    },
+    {
+        path: '/books',
+        component: bookApp
+    },
+    {
+        path: '/book/:bookId',
+        component: bookDetails
+    },
+    {
+        path: '/email',
+        component: emailApp
+    },
+    {
+        path: '/email/:emailId',
+        component: emailDetails
+    },
+    {
+        path: '/notes',
+        component: noteApp
+    },
+    {
+        path: '/note/:noteId',
+        component: noteDetails
+    },
+];
+
+export const myRouter = new VueRouter({ routes: myRoutes })
