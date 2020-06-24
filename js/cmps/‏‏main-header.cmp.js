f193cd88e6603cbc992f5aed006116bb35f0dcbd
dana-factor <factor.dana@gmail.com>
2020-06-24 18:26:17 +0300

    getting started

--- js/cmps/‏‏main-header.cmp.js
+++ js/cmps/‏‏main-header.cmp.js
@@ -1,12 +1,15 @@
 export default {
 	template: `
     <header>
-        <div>Google2</div>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/email">Email</router-link> |
-        <router-link to="/notes">Notes</router-link> |
-        <router-link to="/books">Books</router-link> |
-        <router-link to="/about">About</router-link>
+        <h1>Google2</h1>
+        
+        <nav>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/email">Email</router-link> |
+            <router-link to="/notes">Notes</router-link> |
+            <router-link to="/books">Books</router-link> |
+            <router-link to="/about">About</router-link>
+        </nav>
     </header>
     `,
 };
