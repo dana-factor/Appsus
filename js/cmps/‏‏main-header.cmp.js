6144675bc0cb0fe0539f25ee85e03c52681f9af7
Opal Mizrahi <opal.mizrahi2@gmail.com>
2020-06-24 18:04:49 +0300

    header footer and helpers

---
+++ js/cmps/‏‏main-header.cmp.js
@@ -0,0 +1,12 @@
+export default {
+	template: `
+    <header>
+        <div>Google2</div>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/email">Email</router-link> |
+        <router-link to="/notes">Notes</router-link> |
+        <router-link to="/books">Books</router-link> |
+        <router-link to="/about">About</router-link>
+    </header>
+    `,
+};
