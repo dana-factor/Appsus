4239070ff4b9aeb217b2647aea35cae2a341a4bc
Opal Mizrahi <opal.mizrahi2@gmail.com>
2020-06-24 20:33:44 +0300

    fix

--- js/apps/notes/pages/note-app.cmp.js
+++ js/apps/notes/pages/note-app.cmp.js
@@ -1,9 +1,39 @@
 import { noteService } from '../services/note.service.js';
+import noteFilter from '../cmps/note-filter.cmp.js';
+import noteEdit from '../cmps/note-edit.cmp.js';
+import noteList from '../cmps/note-list.cmp.js';
 
 export default {
 	template: `
         <section>
             <h1>NOTE APP</h1>
+
+            <note-list></note-list>
         </section>
     `,
+	data() {
+		return {
+			notes: null,
+			filterBy: null,
+		};
+	},
+	computed: {
+		// notesToShow() {
+		// 	return notes;
+		// },
+	},
+	methods: {
+		created() {
+			noteService.getNotes().then((notes) => {
+				this.notes = notes;
+			});
+		},
+	},
+	components: {
+        noteList,
+		noteFilter,
+		noteEdit,
+	},
 };
+/* <note-filter></note-filter>
+<note-edit></note-edit> */
--- js/apps/notes/services/note.service.js
+++ js/apps/notes/services/note.service.js
@@ -1,3 +1,46 @@
+// import {utilsService} from '../../../services/utils.service.js';
+import { storageService } from '../../../services/storage.service.js';
+
 export const noteService = {
-    
+	getNotes,
+};
+
+var gNotes = createNotes();
+function createNotes() {
+	let notes = storageService.loadFromStorage('notes');
+	if (!notes || notes.length === 0) {
+		notes = [
+			{
+				type: 'NoteText',
+				isPinned: true,
+				info: {
+					txt: 'Fullstack Me Baby!',
+				},
+			},
+			{
+				type: 'NoteImg',
+				info: {
+					url: 'http://some-img/me',
+					title: 'Me playing Mi',
+				},
+				style: {
+					backgroundColor: '#00d',
+				},
+			},
+			{
+				type: 'NoteTodos',
+				info: {
+					label: 'How was it:',
+					todos: [
+						{ txt: 'Do that', doneAt: null },
+						{ txt: 'Do this', doneAt: 187111111 },
+					],
+				},
+			},
+		];
+	}
+	return notes;
+}
+function getNotes() {
+	return gNotes;
 }
