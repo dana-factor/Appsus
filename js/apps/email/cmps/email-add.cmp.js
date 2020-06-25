40318a98e6cb93b4ad3e2adc0c5fa2c9af3259fd
dana-factor <factor.dana@gmail.com>
2020-06-26 02:03:29 +0300

    compose message

--- js/apps/email/cmps/email-add.cmp.js
+++ js/apps/email/cmps/email-add.cmp.js
@@ -1,5 +1,31 @@
-export default{
-    template: `
-    
+export default {
+	template: `
+        <section class="email-add flex column">
+                <h2>New Message</h2>
+                <input type="email" v-model="emailToSend.sentTo" required placeholder="To: example@appsus.com">
+                <input type="text" v-model="emailToSend.subject" required placeholder="Subject">
+                <textarea type="text" v-model="emailToSend.text" rows="10" required placeholder="Enter text"/>
+                <button @click="sendMail">Send</button>
+        </section>
     `,
-}
+	data() {
+		return {
+			emailToSend: {
+				sentTo: '',
+				sentFrom: 'factor.dana@gmail.com',
+				subject: '',
+				text: '',
+			},
+		};
+	},
+	methods: {
+		sendMail() {
+			console.log(this.emailToSend);
+		},
+    },
+    destroyed(){
+        console.log('draft saved', this.emailToSend);
+            
+        
+    }
+};
--- js/apps/email/cmps/email-preview.cmp.js
+++ js/apps/email/cmps/email-preview.cmp.js
@@ -1,14 +1,28 @@
 export default{
     props: ['email'],
     template: `
-            <li class="email-preview flex">
-                <i :class="isRead" @click="updateEmailRead(email, false)"></i>
+            <li class="email-preview flex space-between align-center">
+                <div>
+                    <i :class="isRead" @click="updateEmailRead(email, false)"></i>
                     <router-link :to="'/email/' + email.id" >
                         <h4 @click="updateEmailRead(email, true)">{{email.sentFrom.name}}:</h4>
-                        <p>{{email.subject}}</p>
-                    </router-link> 
+                        <p> {{email.subject}}</p>
+                        <!-- <p>{{date.getDate()}} - {{date.getMonth()}} - {{date.getFullYear}}</p> -->
+                        
+                    </router-link>
+                </div> 
+                <p>{{date.day}}.{{date.month}}.{{date.year}}</p>
             </li>
       `,
+    data (){
+        return{
+            date:{
+                day: new Date(this.email.sentAt).getDate(),
+                month: new Date(this.email.sentAt).getMonth(),
+                year: new Date(this.email.sentAt).getFullYear()
+            }
+        }
+    },
     computed: {
         isRead(){
             return (this.email.isRead) ? 'far fa-envelope-open' : 'fas fa-envelope';
