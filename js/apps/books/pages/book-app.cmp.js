42493d79aa90f8132d1c768916a4ea1908bbc355
Opal Mizrahi <opal.mizrahi2@gmail.com>
2020-06-27 14:03:21 +0300

    Initial Books

--- js/apps/books/pages/book-app.cmp.js
+++ js/apps/books/pages/book-app.cmp.js
@@ -1,9 +1,78 @@
 import { bookService } from '../services/book.service.js';
+import bookFilter from '../cmps/book-filter.cmp.js';
+import bookList from '../cmps/book-list.cmp.js';
+import bookAdd from '../cmps/book-edit.cmp.js';
+// import bookDetails from '../cmps/book-details.cmp.js';
 
 export default {
 	template: `
-        <section>
-            <h1>BOOK APP</h1>
-        </section>
+        <main class="app-main book-app">
+            <book-filter @filtered="setFilter"></book-filter>
+            <book-add @searched="searchGoogleBook" @added="addGoogleBook":googleBooks = googleBooks></book-add>
+            <!-- <book-details :book="currBook" @close="setCurrBook" v-if="currBook"></book-details> -->
+            <book-list v-if="books" :books="booksToShow"></book-list>
+            <!-- <book-list :books="booksToShow" @bookSelected="setCurrBook" v-else></book-list> -->
+        </main>
     `,
+	data() {
+		return {
+			books: null,
+			filterBy: null,
+			currBook: null,
+			googleBooks: null,
+		};
+	},
+	computed: {
+		booksToShow() {
+			const filterBy = this.filterBy;
+			if (!filterBy) return this.books;
+
+			var filteredBooks = this.books.filter((book) => {
+				return book.title
+					.toLowerCase()
+					.includes(filterBy.searchStr.toLowerCase());
+			});
+			filteredBooks = filteredBooks.filter((book) => {
+				if (!filterBy.priceRange) return true;
+				else if (filterBy.priceRange === 'all') return book;
+				else if (filterBy.priceRange === '1')
+					return book.listPrice.amount <= 75;
+				else if (filterBy.priceRange === '2')
+					return book.listPrice.amount > 75 && book.listPrice.amount <= 150;
+				else if (filterBy.priceRange === '3')
+					return book.listPrice.amount > 150;
+			});
+
+			return filteredBooks;
+		},
+	},
+	methods: {
+		setFilter(filterBy) {
+			this.filterBy = filterBy;
+		},
+		searchGoogleBook(searchStr) {
+			bookService.getGoogleBooks().then((books) => {
+				let filteredBooks = books.items.filter((book) => {
+					return book.volumeInfo.title
+						.toLowerCase()
+						.includes(searchStr.toLowerCase());
+				});
+				this.googleBooks = filteredBooks;
+			});
+		},
+		addGoogleBook(googleBook) {
+			bookService.addGoogleBook(googleBook);
+		},
+	},
+	created() {
+		bookService.getBooks().then((books) => {
+			this.books = books;
+		});
+	},
+	components: {
+		// bookDetails,
+		bookFilter,
+		bookList,
+		bookAdd,
+	},
 };
--- js/apps/books/pages/‏‏book-details.cmp.js
+++ js/apps/books/pages/‏‏book-details.cmp.js
@@ -1,9 +1,25 @@
 import { bookService } from '../services/book.service.js';
+// import bookDetails from '../pages/book-details.cmp.js';
 
 export default {
+	props: ['review'],
 	template: `
-        <section>
-            <h1>BOOK DETAILS</h1>
-        </section>
+    <li>
+        <h4>{{review.writenBy}}'s review:</h4>
+        <h5>Rate:</h5> <p>{{review.rate}}</p>
+        <h5>Read at:</h5> <p>{{review.readAt}}</p>
+        <p>{{review.review}}</p>
+
+    </li>
+        
     `,
+	created() {
+		const { bookId } = this.$route.params;
+		bookService.getBookById(bookId).then((book) => {
+			this.review = book;
+		});
+	},
+	// components: {
+	// 	bookDetails,
+	// },
 };
