fa68795fea7d8239c7f6628fd9093b6b823595aa
Opal Mizrahi <opal.mizrahi2@gmail.com>
2020-06-28 23:34:28 +0300

    menu

--- js/cmps/‏‏main-header.cmp.js
+++ js/cmps/‏‏main-header.cmp.js
@@ -1,16 +1,22 @@
 export default {
 	template: `
     <header class="">
-        <div class="header-container container flex align-center space-between main-header">
+        <div class="header-container container flex align-center space-between main-header"  :class="{'menu-open':isMenuOpen}">
             <h1>onTrip</h1>
             
-            <nav class="flex">
-                <router-link to="/">Home</router-link>
-                <router-link to="/email">Email</router-link>
-                <router-link to="/notes">Notes</router-link>
-                <router-link to="/about">About</router-link>
+            <nav class="flex header-nav">
+                    <router-link to="/">Home</router-link>
+                    <router-link to="/email">Email</router-link>
+                    <router-link to="/notes">Notes</router-link>
+                    <router-link to="/about">About</router-link>
             </nav>
+            <button @click="isMenuOpen=!isMenuOpen" class="menu-btn"></button>
         </div>
     </header>
     `,
+	data() {
+		return {
+			isMenuOpen: false,
+		};
+	},
 };
