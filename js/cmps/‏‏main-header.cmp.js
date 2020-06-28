a2b1689651a7d53549a53414d1a612b10cae00e2
Opal Mizrahi <opal.mizrahi2@gmail.com>
2020-06-28 19:56:04 +0300

    header links

--- js/cmps/‏‏main-header.cmp.js
+++ js/cmps/‏‏main-header.cmp.js
@@ -4,11 +4,11 @@ export default {
         <div class="header-container container flex align-center space-between main-header">
             <h1>onTrip</h1>
             
-            <nav>
-                <router-link to="/">Home</router-link> |
-                <router-link to="/email">Email</router-link> |
-                <router-link to="/notes">Notes</router-link> |
-                <router-link to="/books">Books</router-link> |
+            <nav class="flex">
+                <router-link to="/">Home</router-link>
+                <router-link to="/email">Email</router-link>
+                <router-link to="/notes">Notes</router-link>
+                <!-- <router-link to="/books">Books</router-link> | -->
                 <router-link to="/about">About</router-link>
             </nav>
         </div>
