ef2d770dbd455befe11e1bc2be07f3a8ea93896d
Opal Mizrahi <opal.mizrahi2@gmail.com>
2020-06-28 23:53:35 +0300

    fix

--- js/cmps/‏‏main-header.cmp.js
+++ js/cmps/‏‏main-header.cmp.js
@@ -4,13 +4,13 @@ export default {
         <div class="header-container container flex align-center space-between main-header"  :class="{'menu-open':isMenuOpen}">
             <h1>onTrip</h1>
             
-            <nav class="flex header-nav">
+            <nav class="flex header-nav" @click="toggleMenu">
                     <router-link to="/">Home</router-link>
                     <router-link to="/email">Email</router-link>
                     <router-link to="/notes">Notes</router-link>
                     <router-link to="/about">About</router-link>
             </nav>
-            <button @click="isMenuOpen=!isMenuOpen" class="menu-btn"></button>
+            <button @click="toggleMenu" class="menu-btn"></button>
         </div>
     </header>
     `,
@@ -18,5 +18,10 @@ export default {
 		return {
 			isMenuOpen: false,
 		};
+	},
+	methods: {
+		toggleMenu() {
+			this.isMenuOpen = !this.isMenuOpen;
+		},
 	},
 };
