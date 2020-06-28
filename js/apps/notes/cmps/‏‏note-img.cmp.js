837a6c44842dd00af38a5cfad72519b54d94124c
Opal Mizrahi <opal.mizrahi2@gmail.com>
2020-06-28 17:57:02 +0300

    fix

--- js/apps/notes/cmps/‏‏note-img.cmp.js
+++ js/apps/notes/cmps/‏‏note-img.cmp.js
@@ -3,12 +3,12 @@ export default {
 	props: ['info', 'isEdit'],
 	template: `
           <section class="note-img">
-                <img v-if="info.url" @click="show" :src="info.url" alt="Invalid Image URL"/>
+                <img v-if="info.url" @click="showBigImg" :src="info.url" alt="Invalid Image URL"/>
                 <input v-if="isEdit" type="text" v-model="info.url" placeholder="Enter image url"/>
           </section>
           `,
 	methods: {
-		show() {
+		showBigImg() {
 			eventBus.$emit('showBigImg', this.info.url);
 		},
 	},
--- js/services/eventbus.service.js
+++ js/services/eventbus.service.js
@@ -6,8 +6,8 @@ const bus = new Vue();
 bus.$on('showBigImg', (url) => {
 	Swal.fire({
 		imageUrl: url,
-		imageWidth: '1000',
-		imageHeight: '1000',
+		// imageWidth: '1000',
+		// imageHeight: '1000',
         imageAlt: 'image',
         showConfirmButton: false,
 		showCancelButton: true,
