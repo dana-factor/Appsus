8fbdb4f330b70d12138a3ba68ce3bc98815170cd
dana-factor <factor.dana@gmail.com>
2020-06-28 22:21:08 +0300

    style

--- js/apps/email/cmps/‏‏email-side-nav.cmp.js
+++ js/apps/email/cmps/‏‏email-side-nav.cmp.js
@@ -6,12 +6,12 @@ export default{
         <nav class="email-side-nav flex column">
             <h2>Trip inTouch</h2>
 
-            <router-link to="/email/compose"><button @click="updateDisplay('edit')"> Compose<i class="fas fa-plus"></i></button></router-link>
-            <button @click="updateDisplay('listAndFilter'); setDisplayGroup('incoming')">Inbox [{{unreadCount}}] <i class="fas fa-inbox"></i> </button>
+            <router-link to="/email/compose"><button @click="updateDisplay('edit')"><span>Compose </span><i class="fas fa-plus"></i></button></router-link>
+            <button @click="updateDisplay('listAndFilter'); setDisplayGroup('incoming')"><span>Inbox [{{unreadCount}}] </span><i class="fas fa-inbox"></i></button>
             <!-- <button @click="showEmails"><i class="fas fa-inbox"></i>Inbox ({{unreadCount}})</button> -->
-            <button @click="setDisplayGroup('stared'); updateDisplay('listAndFilter')">Stared <i class="fas fa-star"></i></button>
-            <button @click="setDisplayGroup('outgoing'); updateDisplay('listAndFilter')">Sent Mail <i class="far fa-paper-plane"></i></button>
-            <button @click="setDisplayGroup('draft'); updateDisplay('listAndFilter')">Drafts <i class="fab fa-firstdraft"></i></button>
+            <button @click="setDisplayGroup('stared'); updateDisplay('listAndFilter')"><span>Stared </span><i class="fas fa-star"></i></button>
+            <button @click="setDisplayGroup('outgoing'); updateDisplay('listAndFilter')"><span>Sent Mail </span><i class="far fa-paper-plane"></i></button>
+            <button @click="setDisplayGroup('draft'); updateDisplay('listAndFilter')"><span>Drafts </span><i class="fab fa-firstdraft"></i></button>
             <email-status></email-status>
         </nav>
     `,
